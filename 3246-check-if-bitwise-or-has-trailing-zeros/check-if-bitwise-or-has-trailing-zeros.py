26c7528e3e893490895cb94d4bd92bf54b5dea3f
edith-farzana <farzanaedith@gmail.com>
2026-09-04 14:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (50.38%) - LeetSync

---
+++ 3246-check-if-bitwise-or-has-trailing-zeros/check-if-bitwise-or-has-trailing-zeros.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasTrailingZeros(self, nums: list[int]) -> bool:
+        even_count = 0
+        for num in nums:
+            if num % 2 == 0:
+                even_count += 1
+                if even_count >= 2:
+                    return True
+        return False
