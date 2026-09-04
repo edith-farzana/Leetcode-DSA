f630b9e1287facb14072c0bf6e5911400f2942bf
edith-farzana <farzanaedith@gmail.com>
2026-09-04 14:08:38 +0530

    Time: 50 ms (98.67%) | Memory: 19.3 MB (73.33%) - LeetSync

--- 2275-find-substring-with-given-hash-value/find-substring-with-given-hash-value.py
+++ 2275-find-substring-with-given-hash-value/find-substring-with-given-hash-value.py
@@ -1,27 +1,40 @@
+import math
 class Solution:
     def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
-        n = len(s)
-        current_hash = 0
-        pk_minus_1 = pow(power, k - 1, modulo)
-        
-        for i in range(n - k, n):
-            val = ord(s[i]) - ord('a') + 1
-            current_hash = (current_hash + val * pow(power, i - (n - k), modulo)) % modulo
-            
-        result_index = -1
-        
-        if current_hash == hashValue:
-            result_index = n - k
-            
-        for i in range(n - k, 0, -1):
-            val_out = ord(s[i + k - 1]) - ord('a') + 1
-            val_in = ord(s[i - 1]) - ord('a') + 1
-            
-            current_hash = (current_hash - val_out * pk_minus_1) % modulo
-            current_hash = (current_hash * power + val_in) % modulo
-            current_hash = (current_hash + modulo) % modulo
-            
-            if current_hash == hashValue:
-                result_index = i - 1
-                
-        return s[result_index : result_index + k]
+        def euler(num: int) -> int:
+            p, res = 3, num
+            if not num&1:
+                res //= 2
+                while not num&1:
+                    num //= 2
+            while num >= p*p:
+                if not num%p:
+                    res = res//p*(p-1)
+                    while not num%p:
+                        num //= p
+                p += 2
+            if num > 1:
+                res = res//num*(num-1)
+            return res
+
+        if math.gcd(power,modulo) == 1:
+            h, pp, inv = 0, 1, pow(power, euler(modulo)-1, modulo)
+            for i in range(k):
+                h = (h + pp * (ord(s[i])-96)) % modulo
+                pp = pp * power % modulo
+            for i,c in enumerate(s[k:]):
+                if h == hashValue:
+                    return s[i:i+k]
+                h = (h+(ord(c)-96)*pp-ord(s[i])+96) % modulo
+                h = h * inv % modulo
+            return s[-k:]
+        else:
+            res, n, h, pp = len(s)-k, len(s), 0, 1
+            for i in range(-k,0):
+                h = (h + pp * (ord(s[i])-96)) % modulo
+                pp = pp * power % modulo
+            for i in range(n-k-1, -1, -1):
+                h = (h*power-(ord(s[i+k])-96)*pp+ord(s[i])-96) % modulo
+                if h == hashValue:
+                    res = i
+            return s[res:res+k]
